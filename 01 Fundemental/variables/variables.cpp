84bf2e216d1118603584633f29c44f72738b05bf
Mark <113889912+kno20205@users.noreply.github.com>
2024-11-13 20:16:44 +0100

    add variables example

---
+++ 01 Fundemental/variables/variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Initializing == Declaring + Assigning
+    int a = 5;
+
+    // Declaring variables
+    int b, c;
+    int result;
+
+    // Assigning values
+    b = 2;
+    c = 4;
+
+    // Process
+    result = a + b + c;
+
+    // print out the result
+    cout << result; // 11
+
+    return 0;
+}
